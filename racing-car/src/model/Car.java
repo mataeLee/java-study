149a926d6de678bd4a4ee6dc4b4942bbe2ea48f6
이태윤 <tyleeg0901@gmail.com>
2020-08-21 13:44:10 +0900

    update car model, remove getter, update standard value, implements comparable, add positionShape

--- racing-car/src/model/Car.java
+++ racing-car/src/model/Car.java
@@ -1,24 +1,40 @@
 package model;
 
-public class Car {
+import standard.Standard;
+
+public class Car implements Comparable<Car>{
     private final String name;
     private int position;
+    private String positionShape;
 
     public Car(String name) {
         this.name = name;
-        this.position = 0;
+        this.position = Standard.CAR_BASE_POSITION;
+        positionShape = Standard.CAR_BASE_POSITION_SHAPE;
     }
 
-    public int getPosition(){
-        return position;
-    }
-
-    public String getName() {
-        return name;
-    }
+//    public int getPosition(){
+//        return position;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
 
     public void behavior(){
-        int val = (int)(Math.random() * 10);
-        this.position += (val > Standard.MOVE_STANDARD)? 1 : 0;
+        int val = (int)(Math.random() * Standard.CAR_MOVE_RANDOM_RANGE);
+        this.position += (val > Standard.MOVE_STANDARD)? Standard.CAR_MOVE_VAL : Standard.CAR_STOP_VAL;
+        this.positionShape += (val > Standard.MOVE_STANDARD)? Standard.CAR_MOVE_POSITION_SHAPE : Standard.CAR_STOP_POSITION_SHAPE;
+    }
+
+    public void showPosition(){
+        System.out.println(this.name + Standard.COLON + positionShape);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        if(this.position > o.position) return 1;
+        else if(this.position == o.position) return 0;
+        else return -1;
     }
 }
