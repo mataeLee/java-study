32c884ad458577d455297a232d6e895764ace7af
이태윤 <tyleeg0901@gmail.com>
2020-08-21 13:41:45 +0900

    update standard static value

--- racing-car/src/standard/Standard.java
+++ racing-car/src/standard/Standard.java
@@ -1,4 +1,4 @@
-package standart;
+package standard;
 
 public class Standard {
     public static final int MOVE_STANDARD = 3;
@@ -10,7 +10,9 @@ public class Standard {
     public static final String INPUT_NAMES_GUIDE_CONTENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_NUM_GUIDE_CONTENT = "시도할 횟수는 몇 회인가요?";
     public static final String SHOW_WINNER_CONTENT = "최종 우승자 : ";
-    public static final String POSITION_SHAPE = "-";
+    public static final String CAR_MOVE_POSITION_SHAPE = "-";
+    public static final String CAR_STOP_POSITION_SHAPE = "";
+    public static final String CAR_BASE_POSITION_SHAPE = "-";
     public static final String INPUT_CONTENT_SPLIT_VAL = ",";
     public static final String RESULT_OUPUT_SPLITTER_VAL = ", ";
     public static final String COLON = " : ";
