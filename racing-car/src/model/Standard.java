eb78592e6e1b468a83e8c38ec8223e18b98fac27
이태윤 <tyleeg0901@gmail.com>
2020-08-21 13:39:01 +0900

    update standard value, remove observer class

--- racing-car/src/model/Standard.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public class Standard {
-    public static final int MOVE_STANDARD = 3;
-}
---
+++ racing-car/src/standard/Standard.java
@@ -0,0 +1,17 @@
+package standart;
+
+public class Standard {
+    public static final int MOVE_STANDARD = 3;
+    public static final int CAR_MOVE_RANDOM_RANGE = 10;
+    public static final int CAR_BASE_POSITION = 0;
+    public static final int CAR_MOVE_VAL = 1;
+    public static final int CAR_STOP_VAL = 0;
+
+    public static final String INPUT_NAMES_GUIDE_CONTENT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_NUM_GUIDE_CONTENT = "시도할 횟수는 몇 회인가요?";
+    public static final String SHOW_WINNER_CONTENT = "최종 우승자 : ";
+    public static final String POSITION_SHAPE = "-";
+    public static final String INPUT_CONTENT_SPLIT_VAL = ",";
+    public static final String RESULT_OUPUT_SPLITTER_VAL = ", ";
+    public static final String COLON = " : ";
+}
